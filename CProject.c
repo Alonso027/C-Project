f782f153971246149a69e6222d73c8f93595d16b
Alonso De La Pena Garcia-Rojo - STUDENT <g00471307@atu.ie>
2025-03-18 10:28:51 +0000

    Added win condition

--- CProject.c
+++ CProject.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+int win(char *grid);
 
 int main()
 {
 
 	return 0;
+}
+
+int win(char* grid)
+{
+	int winner = 0;
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		if (*(grid + i) == *(grid + i + 3) && *(grid + i) == *(grid + i + 6))
+		{
+			winner = 1;
+		}
+	}
+	for (i = 0; i < 6; i+=3)
+	{
+		if (*(grid + i) == *(grid + i + 1) && *(grid + i) == *(grid + i + 2))
+		{
+			winner = 1;
+		}
+	}
+	if (*grid == *(grid + 4) && *grid == *(grid + 8))
+	{
+		winner = 1;
+	}
+	if (*(grid+2) == *(grid + 4) && *(grid+2) == *(grid + 6))
+	{
+		winner = 1;
+	}
+	return winner;
 }
