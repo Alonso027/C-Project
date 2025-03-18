b6e2bc2ff65d837760872a014bcc47d759ae5305
Alvarez Jose Manuel - STUDENT <g00463862@atu.ie>
2025-03-18 09:50:37 +0000

    hola

--- CProject.cpp
+++ CProject.cpp
@@ -7,25 +7,6 @@ using namespace std;
 
 int main()
 {
-	string email;
-	string comp="%20";
-	string phrase = "%20*%20* FROM : student123456@atu.ie %20*%20";
-	int i=0;
-	int count = 0;
-	int find;
-
-	email = phrase.substr(16, 20);
-	cout << "The message is from: " << email << endl;
-
-	for (i = 0; i < phrase.length(); i++)
-	{
-		find = phrase.find(comp, i);
-		if (find == i)
-		{
-			count++;
-		}
-	}
-	cout << "The substring %20 occurs " << count << " times";
 
 	return 0;
 }
