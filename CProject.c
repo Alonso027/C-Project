ade765b7a9bbdb555eccdf808f8efb9f343f6b64
Alonso De La Pena Garcia-Rojo - STUDENT <g00471307@atu.ie>
2025-03-25 09:55:50 +0000

    Added lose and tie conditions and some comments

--- CProject.c
+++ CProject.c
@@ -3,18 +3,20 @@
 #include<time.h>
 #define SIZE 9
 
-int win(char *grid);
+int win(char *grid, char inp);
 
 int main()
 {
-	char arr[SIZE];
+	char arr[SIZE] =  { '0','1','2',
+						'3','4','5',
+						'6','7','8' };
+	int j; //Turns
 	int i; //i=row
 	char userToken;
-	char X, O, opt;
+	char opt;
 
-	printf("Please select your token X or O: ");
+	printf("Please select your token X or O: ");	//here we prompt the user for the token he wants to use
 	scanf_s(" %c" +1, &opt);
-
 	if (opt == 'X'|| opt=='x')
 	{
 		userToken = 'X';
@@ -24,58 +26,113 @@ int main()
 		userToken = 'O';
 	}
 
-	for (i = 0; i < SIZE; i++)
+	for (j = 0; j < 9; j++)
 	{
-		if (i == 2)
+		for (i = 0; i < SIZE; i++) //This loop displays the grid
 		{
-			printf("\n");
-			printf("---------");
-			printf("\n");
+			if (i == 2)
+			{
+				printf("\n");
+				printf("---------");
+				printf("\n");
+			}
+			if (i == 5)
+			{
+				printf("\n");
+				printf("---------");
+				printf("\n");
+			}
+			printf("%c |", arr[i]);
 		}
-		if (i == 5)
+
+		if (j % 2 == 0) //Insert player turn here
 		{
-			printf("\n");
-			printf("---------");
-			printf("\n");
 
 		}
 
-		printf("%c |", arr[i]);
+		else //Insert computer turn here
+		{
+
+		}
+
+		if (win(&arr[0],userToken) == 1) //The function win returns 1 if player won
+		{
+			printf("You win");
+			j = 10;						//Here I set j to 10 to exit the loop but not set it to the inmediate after the loop to make a tie option
+		}
+
+		else if (win(&arr[0],userToken) == 2) //And 2 if the player lost
+		{
+			printf("You lose");
+			j = 10;
+		}
 	}
 
-
-
+	if (j = 9)	 //If the loop finished naturally it means it is a tie
+	{
+		printf("It's a tie!!");
+	}
+	
 
 
 
 	return 0;
 }
 
-int win(char* grid)
+int win(char* grid, char inp)
 {
 	int winner = 0;
 	int i;
+
 	for (i = 0; i < 3; i++)
 	{
 		if (*(grid + i) == *(grid + i + 3) && *(grid + i) == *(grid + i + 6))
 		{
-			winner = 1;
+			if (*(grid + i) == inp)
+			{
+				winner = 1;
+			}
+			else
+			{
+				winner = 2;
+			}
 		}
 	}
 	for (i = 0; i < 6; i+=3)
 	{
 		if (*(grid + i) == *(grid + i + 1) && *(grid + i) == *(grid + i + 2))
 		{
-			winner = 1;
+			if (*(grid + i) == inp)
+			{
+				winner = 1;
+			}
+			else
+			{
+				winner = 2;
+			}
 		}
 	}
 	if (*grid == *(grid + 4) && *grid == *(grid + 8))
 	{
-		winner = 1;
+		if (*grid  == inp)
+		{
+			winner = 1;
+		}
+		else
+		{
+			winner = 2;
+		}
 	}
 	if (*(grid+2) == *(grid + 4) && *(grid+2) == *(grid + 6))
 	{
-		winner = 1;
+		if (*(grid + 2) == inp)
+		{
+			winner = 1;
+		}
+		else
+		{
+			winner = 2;
+		}
 	}
 	return winner;
 }
