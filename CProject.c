cea33d6d87aea313b4f0191818c7b10051aef320
Alonso De La Pena Garcia-Rojo - STUDENT <g00471307@atu.ie>
2025-03-26 15:05:05 +0000

    Added game-like interface

--- CProject.c
+++ CProject.c
@@ -12,8 +12,8 @@ int main()
 						'6','7','8' };
 	int j; //Turns
 	int i; //i=row
-	char userToken;
-	char opt;
+	int w=0, l=0, t=0, ng=0;
+	char userToken, opt, playAgain;
 
 	printf("Please select your token X or O: ");	//here we prompt the user for the token he wants to use
 	scanf_s(" %c" +1, &opt);
@@ -25,57 +25,79 @@ int main()
 	{
 		userToken = 'O';
 	}
-
-	for (j = 0; j < 9; j++)
+	while (ng != 1)	//This loop enables the play again function and only breaks when player stops playing
 	{
-		for (i = 0; i < SIZE; i++) //This loop displays the grid
+		for (j = 0; j <= 9; j++)
 		{
-			if (i == 2)
+			for (i = 0; i < SIZE; i++) //This loop displays the grid
 			{
-				printf("\n");
-				printf("---------");
-				printf("\n");
+				printf("%c |", arr[i]);
+				if (i == 2)
+				{
+					printf("\n");
+					printf("---------");
+					printf("\n");
+				}
+				if (i == 5)
+				{
+					printf("\n");
+					printf("---------");
+					printf("\n");
+				}
 			}
-			if (i == 5)
+
+			if (j % 2 == 0) //Insert player turn here
 			{
-				printf("\n");
-				printf("---------");
-				printf("\n");
+
 			}
-			printf("%c |", arr[i]);
+
+			else //Insert computer turn here
+			{
+
+			}
+
+			if (win(&arr[0], userToken) == 1) //The function win returns 1 if player won
+			{
+				printf("You win");
+				j = 10;			//Here I set j to 10 to exit the loop but not set it to the inmediate after the loop to make a tie option
+				w++;
+			}
+
+			else if (win(&arr[0], userToken) == 2) //And 2 if the player lost
+			{
+				printf("You lose");
+				j = 10;
+				l++;
+			}
 		}
 
-		if (j % 2 == 0) //Insert player turn here
+		if (j = 9)	 //If the loop finished naturally it means it is a tie
 		{
-
+			printf("It's a tie!!");
+			t++;
 		}
 
-		else //Insert computer turn here
+		printf("Wins: %i, Loses: %i, Ties: %i \n Would you like to play again? Y/N\n", w, l, t);
+		scanf_s(" %c", &playAgain);
+		switch (playAgain)
 		{
-
+		case 'N':
+			printf("Goodbye, thanks for playing");
+			ng++;
+			break;
+		case 'n':
+			printf("Goodbye, thanks for playing");
+			ng++;
+			break;
+		case 'Y':
+			break;
+		case 'y':
+			break;
+		default:
+			printf("Please enter a valid character");
 		}
-
-		if (win(&arr[0],userToken) == 1) //The function win returns 1 if player won
-		{
-			printf("You win");
-			j = 10;						//Here I set j to 10 to exit the loop but not set it to the inmediate after the loop to make a tie option
-		}
-
-		else if (win(&arr[0],userToken) == 2) //And 2 if the player lost
-		{
-			printf("You lose");
-			j = 10;
-		}
-	}
-
-	if (j = 9)	 //If the loop finished naturally it means it is a tie
-	{
-		printf("It's a tie!!");
 	}
 	
-
-
-
 	return 0;
 }
 
