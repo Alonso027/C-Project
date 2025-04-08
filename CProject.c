3f064e7b8f4b1efc574c59c5ecd2f22a225d25c3
Alonso De La Pena Garcia-Rojo - STUDENT <g00471307@atu.ie>
2025-04-08 10:23:33 +0100

    Minor bug fixes, added game like interface and moved siplay into a function for order purposes

--- CProject.c
+++ CProject.c
@@ -3,80 +3,119 @@
 #include<time.h>
 #define SIZE 9
 
-int win(char *grid, char inp);
+int win(char* grid, char inp);
+void display(char* grd);
 
 int main()
 {
-	char arr[SIZE] =  { '0','1','2',
-						'3','4','5',
-						'6','7','8' };
 	int j; //Turns
-	int i; //i=row
-	char userToken;
-	char opt;
+	int w = 0, l = 0, t = 0; //Number of wins, loses or ties
+	int ng = 0; //Stays 0 to play a new game and changes to 1 if the user wants to stop playing
+	char userToken, botToken, opt, playAgain = 0;
 
 	printf("Please select your token X or O: ");	//here we prompt the user for the token he wants to use
-	scanf_s(" %c" +1, &opt);
-	if (opt == 'X'|| opt=='x')
+	scanf_s(" %c" + 1, &opt);
+	if (opt == 'X' || opt == 'x')
 	{
 		userToken = 'X';
+		botToken = 'O';
 	}
 	else
 	{
 		userToken = 'O';
+		botToken = 'X';
 	}
 
-	for (j = 0; j < 9; j++)
+	while (ng < 1)	//This loop enables the play again function and only breaks when player stops playing
 	{
-		for (i = 0; i < SIZE; i++) //This loop displays the grid
+		char arr[SIZE] = { '0','1','2',	//Here I reset the game
+						'3','4','5',
+						'6','7','8' };
+
+		display(&arr[0]);
+
+		for (j = 0; j < 9; j++)
 		{
-			if (i == 2)
+			if (j % 2 == 0) //Insert player turn here
 			{
-				printf("\n");
-				printf("---------");
-				printf("\n");
+
 			}
-			if (i == 5)
+
+			else //Insert computer turn here
 			{
-				printf("\n");
-				printf("---------");
-				printf("\n");
+
 			}
-			printf("%c |", arr[i]);
+
+			display(&arr[0]);
+
+			if (win(&arr[0], userToken) == 1) //The function win returns 1 if player won
+			{
+				printf("You win\n");
+				j = 10;			//Here I set j to 10 to exit the loop but not set it to the inmediate after the loop to make a tie option
+				w++;
+			}
+
+			else if (win(&arr[0], userToken) == 2) //And 2 if the player lost
+			{
+				printf("You lose\n");
+				j = 10;
+				l++;
+			}
 		}
 
-		if (j % 2 == 0) //Insert player turn here
+		if (j == 9)	 //If the loop finished naturally it means it is a tie
 		{
-
+			printf("It's a tie!!\n");
+			t++;
 		}
 
-		else //Insert computer turn here
-		{
+		printf("Wins: %i, Loses: %i, Ties: %i \n Would you like to play again? Y/N\n", w, l, t);
+		scanf_s(" %c", &playAgain);
 
-		}
-
-		if (win(&arr[0],userToken) == 1) //The function win returns 1 if player won
+		switch (playAgain)
 		{
-			printf("You win");
-			j = 10;						//Here I set j to 10 to exit the loop but not set it to the inmediate after the loop to make a tie option
-		}
-
-		else if (win(&arr[0],userToken) == 2) //And 2 if the player lost
-		{
-			printf("You lose");
-			j = 10;
+		case 'N':
+			printf("Goodbye, thanks for playing");
+			ng++;
+			break;
+		case 'n':
+			printf("Goodbye, thanks for playing");
+			ng++;
+			break;
+		case 'Y':
+			break;
+		case 'y':
+			break;
+		default:
+			printf("Not a valid character");
 		}
 	}
-
-	if (j = 9)	 //If the loop finished naturally it means it is a tie
-	{
-		printf("It's a tie!!");
-	}
-	
-
-
 
 	return 0;
+}
+
+void display(char* grd)
+{
+	for (int i = 0; i < SIZE; i++) //This loop displays the grid
+	{
+		printf("%c |", *grd+i);
+		if (i == 2)
+		{
+			printf("\n");
+			printf("---------");
+			printf("\n");
+		}
+		if (i == 5)
+		{
+			printf("\n");
+			printf("---------");
+			printf("\n");
+		}
+		if (i == 8)
+		{
+			printf("\n\n");
+		}
+	}
 }
 
 int win(char* grid, char inp)
@@ -98,7 +137,7 @@ int win(char* grid, char inp)
 			}
 		}
 	}
-	for (i = 0; i < 6; i+=3)
+	for (i = 0; i <= 6; i += 3)
 	{
 		if (*(grid + i) == *(grid + i + 1) && *(grid + i) == *(grid + i + 2))
 		{
@@ -114,7 +153,7 @@ int win(char* grid, char inp)
 	}
 	if (*grid == *(grid + 4) && *grid == *(grid + 8))
 	{
-		if (*grid  == inp)
+		if (*grid == inp)
 		{
 			winner = 1;
 		}
@@ -123,7 +162,7 @@ int win(char* grid, char inp)
 			winner = 2;
 		}
 	}
-	if (*(grid+2) == *(grid + 4) && *(grid+2) == *(grid + 6))
+	if (*(grid + 2) == *(grid + 4) && *(grid + 2) == *(grid + 6))
 	{
 		if (*(grid + 2) == inp)
 		{
